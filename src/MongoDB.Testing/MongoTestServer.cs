dadab49f5b8218cc5ab970d924ca24c49d8b8637
Tugberk Ugurlu <tugberk@outlook.com>
2015-02-13 15:43:28 +0000

    initial load

---
+++ src/MongoDB.Testing/MongoTestServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoDB.Testing
+{
+    public class MongoTestServer : IDisposable
+    {
+        public MongoTestServer()
+        {
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MongoExeWrapper
+    {
+    }
+
+    public interface IMongoExeLocator
+    {
+        string Locate();
+    }
+
+    public class MongoExeLocator : IMongoExeLocator
+    {
+        public string Locate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
