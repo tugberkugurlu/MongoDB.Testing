b02ba84911a5a439e32226c253240446adc1d7b6
Tugberk Ugurlu <tugberk@outlook.com>
2015-12-05 15:05:32 +0000

    added License, readme and nuspec file

---
+++ README.md
@@ -0,0 +1,54 @@
+## MongoDB.Testing
+
+MongoDB integration testing helper from .NET projects that stands up a server and a random database on the fly.
+
+## Quick Start
+
+Install the library into your testing project through NuGet:
+
+```
+Install-Package MongoDB.Testing -pre
+```
+
+Write a `mongod.exe` locator:
+
+```csharp
+public class MongodExeLocator : IMongoExeLocator
+{
+    public string Locate()
+    {
+        return @"C:\Program Files\MongoDB\Server\3.0\bin\mongod.exe";
+    }
+}
+```
+
+Integrate it into your tests:
+
+```csharp
+[Test]
+public async Task HasEnoughRating_Should_Throw_InvalidOperationException_When_The_User_Is_Not_Found()
+{
+    using (MongoTestServer server = MongoTestServer.Start(27017, new MongodExeLocator()))
+    {
+        // ARRANGE
+        var collection = server.Database.GetCollection<UserEntity>("users");
+        var service = new MyCounterService(collection);
+        await collection.InsertOneAsync(new UserEntity
+        {
+            Id = ObjectId.GenerateNewId().ToString(),
+            Name = "foo",
+            Rating = 23
+        });
+
+        // ACT, ASSERT
+        Assert.Throws<InvalidOperationException>(
+            () => service.HasEnoughRating(ObjectId.GenerateNewId().ToString()));
+    }
+}
+```
+
+`MongoTestServer.Start` will do the following:
+
+ - Start a `mongod` instance and expose it through the specified port.
+ - Creates a randomly named MongoDB database on the started instance and exposes it through the `MongoTestServer` instance returned from `MongoTestServer.Start` method.
+ - Cleans up the resources, kills the mongod.exe instance when the `MongoTestServer` instance is disposed.
--- src/MongoDB.Testing/Properties/AssemblyInfo.cs
+++ src/MongoDB.Testing/Properties/AssemblyInfo.cs
@@ -2,37 +2,27 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MongoDB.Testing")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("MongoDB integration testing helper from .NET projects")]
 [assembly: AssemblyProduct("MongoDB.Testing")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("MongoDB.Testing.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f1c6d8b-2017-4679-9ef7-ba901ec9b8a7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta-001")]
