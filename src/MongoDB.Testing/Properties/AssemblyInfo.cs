3ca58832238dccaee606169116b4eebaefd2c95a
Tugberk Ugurlu <tugberk@outlook.com>
2016-10-21 13:19:28 +0100

    moved MongoDB.Testing to coreclr

--- src/MongoDB.Testing/Properties/AssemblyInfo.cs
+++ src/MongoDB.Testing/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MongoDB.Testing")]
 [assembly: AssemblyDescription("MongoDB integration testing helper from .NET projects")]
@@ -8,21 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: InternalsVisibleTo("MongoDB.Testing.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: AssemblyCompany("Tugberk Ugurlu")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
 #elif RELEASE
 [assembly: AssemblyConfiguration("RELEASE")]
 #endif
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta-001")]
